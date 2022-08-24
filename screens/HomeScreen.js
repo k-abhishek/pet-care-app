5cdba0d69c6030ae88878c7f246ef54f2a553fa3
k-abhishek <meabhishekumar.ak@gmail.com>
2022-08-24 20:06:17 +0530

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import { View, Text } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
+
+  return (
+    <SafeAreaView>
+      <Text className="text-red-500">HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
